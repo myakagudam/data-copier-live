e5a8a64702167d8bc099dc89887c8a80650cbc9a
myakagudam <pashamarchana24@gmail.com>
2023-11-25 19:35:24 +0800

    Your commit message here

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+DB_DETAILS = {
+    'dev': {
+        'SOURCE_DB': {
+            'DB_TYPE': 'mysql',
+            'DB_HOST': 'localhost',
+            'DB_NAME': 'archana',
+            'DB_USER': 'root',
+            'DB_PASS': 'Ar@88262403@'
+        },
+        'TARGET_DB': {
+            'DB_TYPE': 'postgres',
+            'DB_HOST': 'localhost',
+            'DB_NAME': 'postgres',
+            'DB_USER': 'postgres',
+            'DB_PASS': 'Ar@88262403@'
+        }
+    }
+}
