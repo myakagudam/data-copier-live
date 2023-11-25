82b37e9ddb3d3a50b43cdad1806da6f71b05d61f
myakagudam <pashamarchana24@gmail.com>
2023-11-25 23:34:34 +0800

    Added new feature

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import sys
+import sys
 from config import DB_DETAILS
 
+from util import get_tables
+
+
 def main():
-    """program takes at least one argument"""
-    env = sys.argv[1]
-    db_details = DB_DETAILS[env]
-    print(db_details)
+   env=sys.argv[1]
+    db_details=DB_DETAILS[env]
+    tables =get_tables('table_list')
+    for table in tables['table_name']:
+        print(table)
 
+    
 if __name__ == '__main__':
-  main()
-
-  
+    main()  
+    
